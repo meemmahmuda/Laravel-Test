579956dc0e882c0a97a9dbb9b04ef72ff66491b5
meemmahmuda <meemmahmuda85@gmail.com>
2024-08-15 05:56:46 +0600

    up

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CustomerController; 
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcom');
 });
 
 Route::get('/customer', [CustomerController::class, 'index'])->name('customer'); 
