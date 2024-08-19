e48924f797c02d20ddedf09c410813754ffd11d2
meemmahmuda <meemmahmuda85@gmail.com>
2024-08-19 10:29:38 +0600

    up

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/customer', [CustomerController::class, 'index'])->name('customer'); 
+Route::get('/', [CustomerController::class, 'index'])->name('customer'); 
 
 Route::get('createcustomer', [CustomerController::class, 'create'])->name('customercreate');
 
